f8df1950c9699f64efb5d40f78ccc72cc21e5e4a
baos1 <onealbao@gmail.com>
2020-06-14 22:26:57 -0500

    change version to 0.12.5.2. for pypi release purpose. setup.py is updated.

--- setup.py
+++ setup.py
@@ -2,21 +2,21 @@ import setuptools
 
 long_description = open("README.rst").read()
 #Version change by Shunxing
-VERSION = "0.12.5.1"
+VERSION = "0.12.5.2"
 
 setup_params = dict(
     name="CacheControl",
     version=VERSION,
-    author="Eric Larson",
-    author_email="eric@ionrock.org",
-    url="https://github.com/ionrock/cachecontrol",
-    keywords="requests http caching web",
+    author="Shunxing Bao",
+    author_email="onealbao@gmail.com",
+    url="https://github.com/VUIIS/cachecontrol",
+    keywords="requests http caching web - ldax testing version",
     packages=setuptools.find_packages(),
     package_data={"": ["LICENSE.txt"]},
     package_dir={"cachecontrol": "cachecontrol"},
     include_package_data=True,
     description="httplib2 caching for requests",
-    long_description=long_description,
+    long_description='''The package is hosted at VUIIS github repo, which is a customized package to enable ldax cahce feature (testing only). The project is orginal hosted at https://github.com/ionrock/cachecontrol''',
     install_requires=["requests", "msgpack>=0.5.2"],
     extras_require={"filecache": ["lockfile>=0.9"], "redis": ["redis>=2.10.5"]},
     entry_points={"console_scripts": ["doesitcache = cachecontrol._cmd:main"]},
